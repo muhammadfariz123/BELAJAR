fd1e2faed4cae36df69c7e959bad0f9efb3ae1a8
muhammadfariz123 <warungkombas@gmail.com>
2025-03-22 05:11:15 +0700

    belajar scope

--- belajar javascript/scope/script.js
+++ belajar javascript/scope/script.js
@@ -6,9 +6,31 @@
 var a = 1;
 
 // contoh function scope = variable b hanya di gunakan di dalam function saja
-//  didalam function bisa mengakses varia
+//  didalam function bisa mengakses variable global scope
 function tes () {
     var b = 2;
 }
 
 tes();
+
+
+
+// contoh name conflict
+// global scope / window scope
+var a = 1;
+
+// contoh function scope = variable b hanya di gunakan di dalam function saja
+//  didalam function bisa mengakses variable global scope
+function tes () {
+    // ini name conflict
+    var a = 2;
+    // memanggil variable global scope
+    console.log(window.a);
+}
+
+tes();
+console.log(a);
+
+// kalau pakai var yang diakses global 
+// kalau ga ada var  yang di akses global maka akan di ambil dari function scope
+// kalau ingin membuat variable lokal di dalam function harus deklarasi dengan var
