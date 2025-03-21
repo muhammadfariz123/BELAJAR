cdfc8199147565496a815ae4103cd5cff53c6407
muhammadfariz123 <warungkombas@gmail.com>
2025-03-21 22:51:46 +0700

    belajar refactoring

---
+++ belajar javascript/refactoring/script.js
@@ -0,0 +1,9 @@
+// refactoring : sebuah proses mengubah kode agar menjadi lebih baik tanpa megubah fungsionalitasnya
+// atau bisa di sebut penulisan kode yang rapi dan efisien,alasan refactoring:
+// 1. Readabilty : kemudahan dalam membaca dan dipahami oleh diri sendiri maupun orang lain
+// 2. maintainability: mudah di perbaiki
+// 3. scalability: mudah diperluas
+// 4. reusability: mudah digunakan ulang
+// 5. refactoring : penulisan kode yang rapi
+// 6. testability : penulisan kode agar mudah di test
+// 7. performance : performa kode yang baik
