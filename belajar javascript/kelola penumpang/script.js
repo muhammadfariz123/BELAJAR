b9bc3e44b3f1a40a194f928e5d063fa2d7b38ded
muhammadfariz123 <warungkombas@gmail.com>
2025-03-28 13:09:19 +0700

    menambahkan fungsi hapus penumpang

--- belajar javascript/kelola penumpang/script.js
+++ belajar javascript/kelola penumpang/script.js
@@ -10,7 +10,7 @@ var tambahPenumpang = function(namaPenumpang, penumpang) {
         // telusuri seluruh kursi dari awal
         for (var i = 0; i < penumpang.length; i++) {
             // jika ada kursi kosong
-            if (penumpang[i] == un) {
+            if (penumpang[i] == undefined) {
                 // tambah penumpang di kursi tersebut
                 penumpang[i] = namaPenumpang;
                 // kembalikan isi array & keluar dari function
@@ -33,6 +33,31 @@ var tambahPenumpang = function(namaPenumpang, penumpang) {
                 }
 
             } 
+        }
+    }
+}
+
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+    // jika angkot kosong
+    if (penumpang.length == 0) {
+        // tampilkan pesan kesalahannya
+        console.log("Angkot kosong, tidak ada penumpang untuk dihapus");
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+    } else {
+        // telusuri seluruh kursi dari awal
+        for (var i = 0; i < penumpang.length; i++) {
+            // jika ada nama yang sama
+            if (penumpang[i] == namaPenumpang) {
+                // hapus penumpang di kursi tersebut
+                penumpang[1] = undefined;
+                return penumpang;
+            } else if ( i == penumpang.length - 1) {
+                // jika tidak ada nama yang sama
+                // lanjutkan pencarian ke kursi berikutnya
+                console.log(namaPenumpang + "tidak ada di dalam angkot");
+                return penumpang;
+            }
         }
     }
 }
