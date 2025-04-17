382c1686be1fc03bbb62a59733949d3d32546cd0
muhammadfariz123 <warungkombas@gmail.com>
2025-04-17 11:17:59 +0700

    menambahkan fungsi query selector dan query selector all

--- belajar javascript/DOM/script.js
+++ belajar javascript/DOM/script.js
@@ -1,26 +1,42 @@
-// DOM Selection Method
-// getElementById() => element
-const judul = document.getElementById('judul');
+// // DOM Selection Method
+// // getElementById() => element
+// const judul = document.getElementById('judul');
 
-judul.style.color = 'white'
-judul.style.backgroundColor = 'grey';
-judul.innerHTML = 'Belajar DOM';
+// judul.style.color = 'white'
+// judul.style.backgroundColor = 'grey';
+// judul.innerHTML = 'Belajar DOM';
 
-// getElementsByTagName() => HTMLCollection
-const paragraf = document.getElementsByTagName('p');
 
-for ( let i = 0; i < paragraf.length; i++)
-{
-    paragraf[i].style.backgroundColor = 'lightblue';
+// // getElementsByTagName() => HTMLCollection
+// const paragraf = document.getElementsByTagName('p');
+
+// for ( let i = 0; i < paragraf.length; i++)
+// {
+//     paragraf[i].style.backgroundColor = 'lightblue';
+// }
+
+// const h1 = document.getElementsByTagName ('h1')[0];
+// h1.style.fontSize = '50px';
+
+
+// // getElementsByClassName() = HTMLCollection
+// const p1 =getElementsByClassName('p1')[0];
+// p1.innerHTML = 'ini diubah dari javascript';
+
+
+// // document.querySelector() = element
+const p4  = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'lightblue';
+
+// const p = document.querySelector('p');
+// p.innerHTML = "ini diubah melalui javascript";
+
+// // querySelectorAll() = nodeList
+const p = document.querySelectorAll('p');
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'lightblue';
 }
-
-const h1 = document.getElementsByTagName ('h1')[0];
-h1.style.fontSize = '50px';
-
-// getElementsByClassName() = HTMLCollection
-const p1 =getElementsByClassName('p1')[0];
-p1.innerHTML = 'ini diubah dari javascript';
-// querySelector() = element
-// querySelectorAll() = nodeList
-
-// document.getElementsByTagName()
