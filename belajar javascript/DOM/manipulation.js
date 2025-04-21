e536f64f74c6cd1ce6e7b145d328d857cc885db0
muhammadfariz123 <warungkombas@gmail.com>
2025-04-21 09:40:48 +0700

    menambahkan manipulasi classlist

--- belajar javascript/DOM/manipulation.js
+++ belajar javascript/DOM/manipulation.js
@@ -30,15 +30,46 @@ a.getAttribute('href');
 judul.getAttribute('id');
 
 // element.removeAttribute() => menghapus attribute yang ada di element
-a.removeAttribute('href');
+// a.removeAttribute('href');
+
+// ================================
+
+// element.classList => memanipulasi class yang ada
+// element.classList.add(); => menambahkan class baru
+const p2 = document.querySelector('.p2');
+p2.classList.add('label');
 
 
 
 
+// element.classList.remove(); => menghapus class yang ada
+p2.classList.remove('label');
+
+
+
+// element.classList.toggle(); => jika sebuah element tidak memilki class tertentu maka dia akan menambahkan, tetapi jika element tersebut sudah punya maka toogle akan menghilangkan class tersebut 
+p2.classList.toggle('label');
+
+// element.classList.item(); => untuk mengetahui kelas tertentu pada sebuah elemen
+p2.classList.add('satu')
+p2.classList.add('dua')
+p2.classList.add('lima')
+console.log(p2.classList.item(1)); // akan menampilkan 'dua'
+
+
+// element.classList.contains(); =>
+// dalam sebuah element punya ga kelas tertentu
+console.log(
+    p2.classList.contains('empat')
+) // akan menampilkan false
+
+
+
+// element.classList.replace(); =>
+// untuk mengganti kelas yang ada dengan kelas yang baru
+p2.classList.replace('dua', 'tiga'); // akan mengganti kelas 'dua' dengan 'tiga'
+console.log(p2.classList.contains('dua')); // akan menampilkan false
 
 
 
 
-
-
-// element.classList
